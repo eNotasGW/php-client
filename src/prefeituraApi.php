291cfdbe03adac460233d83449f47497fff7f54d
Jonathan Souza <jonathansouza23@gmail.com>
2016-11-26 12:02:01 -0200

    Create prefeituraApi.php

---
+++ src/prefeituraApi.php
@@ -0,0 +1,26 @@
+<?php
+	namespace eNotasGW\Api;
+
+	class prefeituraApi extends eNotasGWApiBase {
+		public function __construct($proxy) {
+			parent::__construct($proxy);
+		}
+
+		/**
+		* Consulta as características de uma determinada prefeitura
+		* 
+		*@param int codigoIbge código ibge da cidade cuja a prefeitura será consultada
+		* @return mixed contendo as características da prefeitura em questão
+		*/
+		public function consultar($codigoIbge) {
+			return $this->callOperation(array(
+				'path' => '/estados/cidades/{codigoIbge}/provedor',
+				'parameters' => array(
+					'path' => array(
+						'codigoIbge' => $codigoIbge
+					)
+				)
+			));
+		}
+	}
+?>
